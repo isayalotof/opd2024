88ecfa6b26fda0d0f5cdc608de0fac68b78affea
isayalotof <arg@gmail.com>
2024-01-19 21:47:33 +1000

    abc

--- zadaci.py
+++ zadaci.py
@@ -1,11 +1,15 @@
 def check():
+    """считываю кол-во групп"""
     n = int(input())
     for i in range(n):
+        """считываю кол-во студентов в группе"""
         n1 = int(input())
         students = []
+        """считываю студентов и сохраняю кол-во предметов"""
         for j in range(n1):
             fio, kol = input().split()
             students.append(kol)
+        """проверяю все ли сдали предметы"""
         if '0' in students:
             return 'НЕТ'
     return 'ДА'
