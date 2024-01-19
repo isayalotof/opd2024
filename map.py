bf1af9d5dc043141e41d09e1bb0a38acde2b9718
isayalotof <arg@gmail.com>
2024-01-19 21:54:59 +1000

    abc

--- map.py
+++ map.py
@@ -1,1 +1,16 @@
-print()
+
+
+"""создаю функцию"""
+def simple_map(function, values):
+    """создаю пустой список"""
+    result = []
+    """перебираю все элементы"""
+    for value in values:
+        """добавляю измененные элементы"""
+        result.append(function(value))
+    return result
+
+
+values = [1, 3, 1, 5, 7]
+operation = lambda x: x + 5
+print(*simple_map(operation, values))
